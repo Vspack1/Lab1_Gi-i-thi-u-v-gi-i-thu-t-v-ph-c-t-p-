b2b1837a4686e01b3df550a640f5463bb97c4248
Vspack1 <143935083+Vspack1@users.noreply.github.com>
2026-03-05 17:01:44 +0700

    bai4.cpp

---
+++ bai4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int left = 0;
+    int right = s.length() - 1;
+
+    bool check = true;
+
+    while(left < right) {
+        if(s[left] != s[right]) {
+            check = false;
+            break;
+        }
+        left++;
+        right--;
+    }
+
+    if(check)
+        cout << "YES";
+    else
+        cout << "NO";
+}
+//O(n)
