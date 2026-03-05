dbc328ce55b99b34bff74dcfa25c20f12cff83f9
Vspack1 <143935083+Vspack1@users.noreply.github.com>
2026-03-05 17:00:01 +0700

    bai1.cpp

---
+++ bai1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, x;
+    int a[100];
+    int count = 0;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+
+    cin >> x;
+
+    for(int i = 0; i < n; i++) {
+        if(a[i] == x)
+            count++;
+    }
+
+    cout << count;
+}
